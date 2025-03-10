75a7ce860c1c1ef7af50f54c6dbc05bb565b8f7b
Jennifer108 <126683516+Jennifer108@users.noreply.github.com>
2025-03-10 22:31:42 +0800

    Add files via upload

--- train_run.sh
+++ train_run.sh
@@ -1,14 +1,12 @@
-# 编译 Reg loss
+# Reg loss
 # cd wrapper/bilateralfilter
 # swig -python -c++ bilateralfilter.i   
 # python setup.py install
 
 
 
-
-# now
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2  dist_train_voc_seg_negv2-Memorybank.py
-# CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2  dist_train_coco_seg_negv2-Memorybank.py
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2  train_voc_seg.py
+# CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2  train_coco_seg.py
 
 
 
